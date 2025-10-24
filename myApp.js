4757d0a015a5820104204654e4488a5605fa4b02
yishakt4 <halieseliase1936@gmail.com>
2025-10-25 01:03:57 +0300

    1

--- myApp.js
+++ myApp.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-
-// add this:
 const helmet = require('helmet');
 
-// other middleware you already have...
-app.use(helmet());   // enable helmet early, before your routes
+// Enable Helmet
+app.use(helmet());
 
-// your routes / other app code...
+// Remove the X-Powered-By header
+app.use(helmet.hidePoweredBy());
+
+// Example route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// don't change other code that the challenge provided
 module.exports = app;
